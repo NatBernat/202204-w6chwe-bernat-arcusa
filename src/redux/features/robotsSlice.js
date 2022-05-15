2b4bb3c684945e541968be066f89703988187d3c
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-15 16:05:41 +0200

    Added redux store file

--- src/redux/features/robotsSlice.js
+++ src/redux/features/robotsSlice.js
@@ -4,10 +4,9 @@ const robotsSlice = createSlice({
   name: "robots",
   initialState: [],
   reducers: {
-    loadRobots: (state, action) => [...action.payload],
+    loadRobots: (state, action) => action.payload,
   },
 });
 
 export const { loadRobots: loadRobotsActionCreator } = robotsSlice.actions;
-
-export const projectsReducers = robotsSlice.reducers;
+export const robotsReducer = robotsSlice.reducers;
