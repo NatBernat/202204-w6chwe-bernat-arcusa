d8c8c87b8b2a27b94f129679b5b42f9e54c408bb
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-15 14:03:06 +0200

    Added snapshot test for RobotsList component

---
+++ src/components/RobotsList/RobotsList.test.js
@@ -0,0 +1,32 @@
+import TestRenderer from "react-test-renderer";
+import RobotsList from "./RobotsList";
+
+describe("Given a RobotsList component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should match this snapshot", () => {
+      const robots = [
+        {
+          _id: "627e93e63ccb21d8cae171b2",
+          name: "Terminator",
+          image: "https://m.media-amazon.com/images/I/51hm4aPEGGL._SY445_.jpg",
+          speed: 2,
+          resistance: 8,
+          creation: 1984,
+        },
+        {
+          _id: "627e96883ccb21d8cae171b3",
+          name: "Optimus Prime",
+          image:
+            "https://pm1.narvii.com/7617/0e3da39327724c32a5639bae88d64ad9c60dfecer1-752-1131v2_hq.jpg",
+          speed: "5",
+          resistance: "7",
+          creation: "1973",
+        },
+      ];
+
+      const renderedList = TestRenderer.create(<RobotsList robots={robots} />);
+
+      expect(renderedList).toMatchSnapshot();
+    });
+  });
+});
