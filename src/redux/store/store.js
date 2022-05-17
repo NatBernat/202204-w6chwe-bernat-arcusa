f1c2bd27a658f2209aac9f2e5bea41def5b107f0
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-17 23:10:01 +0200

    Added users login thunk, slice, form and app form call, not working

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import robotsReducer from "../features/robotsSlice";
+import usersReducer from "../features/robotsSlice";
 
 const store = configureStore({
   reducer: {
     robots: robotsReducer,
+    users: usersReducer,
   },
 });
 
